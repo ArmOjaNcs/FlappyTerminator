93f2de0ecc34826cf92f55404041100eeb72df77
ArmOjaNcs <tverarmen@gmail.com>
2025-01-31 09:54:52 +0300

    Fixed

--- Assets/Scripts/LeadersBoard/LeadersBoardInitializer.cs
+++ Assets/Scripts/LeadersBoard/LeadersBoardInitializer.cs
@@ -2,12 +2,6 @@ public static class LidersBoardInitializer
 {
     public readonly static int MaxElements = 10;
 
-    private readonly static string[] _names = new string[]
-    {
-        "Kevin", "Angela", "Robert", "Maria", "Sam",
-        "Betty", "Max", "Samantha", "Nicholas", "Catherine"
-    };
-
     private readonly static int _maxScore = 2200;
     private readonly static int _subtractor = 200;
 
@@ -15,10 +9,22 @@ public static class LidersBoardInitializer
     {
         int currentScore = _maxScore;
         PlayerScore[] newPlayersScores = new PlayerScore[MaxElements];
+        string[] names = GetNames();
 
         for (int i = 0; i < MaxElements; i++)
-            newPlayersScores[i] = new PlayerScore(_names[i], currentScore -= _subtractor);
+            newPlayersScores[i] = new PlayerScore(names[i], currentScore -= _subtractor);
 
         return newPlayersScores;
+    }
+
+    private static string[] GetNames()
+    {
+        string[] names = new string[]
+        {
+        "Kevin", "Angela", "Robert", "Maria", "Sam",
+        "Betty", "Max", "Samantha", "Nicholas", "Catherine"
+        };
+
+        return names;
     }
 }
